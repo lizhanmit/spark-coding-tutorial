34e3bb85cd2ec0bef698d435721c9643da951f7c
Zhan Li <zhan.li.dev@outlook.com>
2019-01-06 18:29:58 +1100

    Added Udemy advanced tutorial code.

---
+++ src/main/scala/com/udemy/tutorial/advanced/MostPopularSuperhero.scala
@@ -0,0 +1,67 @@
+package com.udemy.tutorial.advanced
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+
+/** Find the superhero with the most co-appearances. */
+object MostPopularSuperhero {
+ 
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+   
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+     // Create a SparkContext using every core of the local machine
+    val spark = SparkSession
+      .builder()
+      .appName("MostPopularSuperhero")
+      .master("local[*]")
+      .getOrCreate()
+    val sc = spark.sparkContext 
+    
+    // Build up a hero ID -> name RDD
+    val names = sc.textFile("src/main/resources/udemy/inputData/marvel-names.txt")
+    val namesRdd = names.flatMap(parseNames)
+    
+    // Load up the superhero co-apperarance data
+    val lines = sc.textFile("src/main/resources/udemy/inputData/marvel-graph.txt")
+    
+    // Convert to (heroID, number of connections) RDD
+    val pairings = lines.map(countCoOccurences)
+    
+    // Combine entries that span more than one line
+    val totalFriendsByCharacter = pairings.reduceByKey( (x,y) => x + y )
+    
+    // Flip it to # of connections, hero ID
+    val flipped = totalFriendsByCharacter.map( x => (x._2, x._1) )
+    
+    // Find the max # of connections
+    // result is (maxConnectionNumber, heroID)
+    val mostPopular = flipped.max()
+    
+    // Look up the name (lookup returns an array of results, so we need to access the first result with (0)).
+    val mostPopularName = namesRdd.lookup(mostPopular._2)(0)
+    
+    // Print out our answer!
+    println(s"$mostPopularName is the most popular superhero with ${mostPopular._1} co-appearances.") 
+  }
+  
+    // Function to extract the hero ID and number of connections from each line
+  def countCoOccurences(line: String) = {
+    var elements = line.split("\\s+")
+    ( elements(0).toInt, elements.length - 1 )
+  }
+  
+  // Function to extract hero ID -> hero name tuples (or None in case of failure)
+  def parseNames(line: String) : Option[(Int, String)] = {
+    var fields = line.split('\"')
+    if (fields.length > 1) {
+      return Some(fields(0).trim().toInt, fields(1))
+    } else {
+      return None // flatmap will just discard None results, and extract data from Some results.
+    }
+  }
+}
